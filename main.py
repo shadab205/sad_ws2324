d55555ef88d183d80db5a0988fe49a9d0f88968b
sbstek <shadabsshaikh205@gmail.com>
2023-11-29 11:11:21 +0100

    Added turn Code with buttons

--- main.py
+++ main.py
@@ -5,18 +5,31 @@ from pybricks.ev3devices import TouchSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
+from pybricks.parameters import Button
+from pybricks import ev3brick as brick
+
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
 
 # Initialize the motors.
-motor = Motor(Port.A)
+motor_drive = Motor(Port.A)
+motor_turn  = Motor(Port.B)
 sensor = TouchSensor(Port.S1)
 while True:
+    b = brick.buttons()
+    if Button.LEFT in b:
+        motor_turn.dc(-50)
+
+    elif Button.RIGHT in b:
+        motor_turn.dc(50)
+    else:
+        motor_turn.dc(0)
+
     if sensor.pressed() is True:
         sensor_val = 'True' 
-        motor.dc(50)
+        motor_drive.dc(-50)
     else:
         sensor_val = 'False'
-        motor.dc(0)
-    print('Speed ' + str(motor.speed()) + ', Angle '+ str(motor.angle())+', Sensor 1 '+ sensor_val)
+        motor_drive.dc(0)
+    print('Speed ' + str(motor_drive.speed()) + ', Angle '+ str(motor_drive.angle())+', Sensor 1 '+ sensor_val)
     wait(100)
