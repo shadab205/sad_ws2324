ed9435762126fd310df0ce9ffebb87e89c994a54
sbstek <shadabsshaikh205@gmail.com>
2023-12-04 22:42:56 +0100

    Added State machine code and added placeholder code for drive

--- main.py
+++ main.py
@@ -7,29 +7,58 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 from pybricks.parameters import Button
 from pybricks import ev3brick as brick
+import math
 
+# State machine init start
+
+from state_machine import StateMachine
+
+
+b = brick.buttons()
+sm = StateMachine()
+
+# State machine init end
+
+two_motors = False
 # Initialize the EV3 Brick.
 ev3 = EV3Brick()
 
 # Initialize the motors.
 motor_drive = Motor(Port.A)
-motor_turn  = Motor(Port.B)
+
+if two_motors == True:
+    motor_turn  = Motor(Port.B)
 
 while True:
+
     b = brick.buttons()
-    if Button.UP in b:
-        motor_turn.dc(-50)
-
-    elif Button.DOWN in b:
-        motor_turn.dc(50)
+    if Button.CENTER in b:
+        sm.receive_input_event("button_center")
     else:
-        motor_turn.dc(0)
-
+        sm.receive_input_event("no_event")
+    sm.run()
+    if sm.current_state == "s_init_0":
+        print("we are in init")
+    elif sm.current_state == "s_man_mode":
+        print("we are in man_mode")
+    wait(1000)
+'''
+while True:
+    b = brick.buttons()
+    if two_motors == True:
+        if Button.UP in b:
+            motor_turn.dc(-50)
+        elif Button.DOWN in b:
+            motor_turn.dc(50)
+        else:
+           motor_turn.dc(0)
+    
     if Button.LEFT in b:
-                motor_drive.dc(50)
+        motor_drive.dc(50)
     elif Button.RIGHT in b:
         motor_drive.dc(-30)
     else:
         motor_drive.dc(0)
     print('Speed ' + str(motor_drive.speed()) + ', Angle '+ str(motor_drive.angle()))
     wait(100)
+'''
---
+++ state_machine.py
@@ -0,0 +1,80 @@
+class StateMachine:
+    def __init__(self):
+        self.states = ["s_init_0", "s_man_mode"]
+        self.transitions = [
+            {"current_state": "s_init_0", "event": "button_center", "next_state": "s_man_mode"},
+            {"current_state": "s_man_mode", "event": "button_center", "next_state": "s_init_0"},
+            ]
+        self.events = ["button_center","button_left","button_right","no_event"]
+        self.current_state = self.states[0]
+        self.is_running = True
+        self.input_event = "no_event"
+        
+    def transition(self, event):
+        for t in self.transitions:
+            if t["current_state"] == self.current_state and t["event"] == event:
+                self.current_state = t["next_state"]
+                return True
+        return False
+
+    def run(self):
+        #while self.is_running:
+        print("-----------------------------------------")
+        print("Current state:" + str(self.current_state))
+
+        event = self.get_valid_event()
+        print("event="+event)
+        if not self.transition(event):
+            print("Invalid event. No transition occurred.")
+            
+        self.is_running = self.check_termination_condition()
+        print("-----------------------------------------")    
+    def get_valid_event(self):
+        while True:
+            parsed_event = self.input_event
+            print("parsed_event="+parsed_event)
+            if parsed_event is not None:
+                return parsed_event
+            else:
+                return "no_event"
+
+    def check_termination_condition(self):
+        return True
+    
+    def s_init_0_operation():
+        print("Performing init_0 operation")
+
+    def s_man_mode_operation():
+        print("Performing man_mode operation")
+        
+    def receive_input_event(self,user_input):
+        self.input_event=user_input
+        print("User input="+self.input_event)
+        """
+class StateTransitions:
+    @staticmethod
+    def get_transitions():
+        return [
+            {"current_state": "s_init_0", "event": "button_center", "next_state": "s_man_mode"},
+            {"current_state": "s_man_mode", "event": "button_center", "next_state": "s_init_0"},
+            ]
+        
+class RoverStates:
+    @staticmethod
+    def get_states():
+        return ["init_0", "man_mode"]
+
+class Events:
+    @staticmethod
+    def get_events():
+        return ["button_center","button_left","button_right"]
+
+    @staticmethod
+    def parse_event(user_input):
+        events = Events.get_events()
+        if user_input in events:
+            return user_input
+        else:
+            print("Invalid event. Please enter a valid event.")
+            return None
+"""        
